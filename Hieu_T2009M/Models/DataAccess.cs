6e257d70a885a4cd74974c6b8c089ba14b52bb8c
mylaptop <hieuvanphan275@gmail.com>
2021-11-09 10:03:49 +0700

    Add project files.

---
+++ Hieu_T2009M/Models/DataAccess.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace Hieu_T2009M.Models
+{
+    class DataAccess
+    {
+        public async static void InitializeDatabase()
+        {
+            await ApplicationData.Current.LocalFolder.CreateFileAsync(
+                "sqliteSample.db", CreationCollisionOption.OpenIfExists
+                );
+            string dbpath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "sqliteSample.db");
+            using (SqliteConnection db =
+               new SqliteConnection($"Filename={dbpath}"))
+            {
+                db.Open();
+                String tableCommand = "CREATE TABLE IF NOT " +
+                    "EXISTS Contact (phone_ INTEGER PRIMARY KEY, " +
+                    "name NVARCHAR(250) NULL)";
+                SqliteCommand createTable = new SqliteCommand(tableCommand, db);
+                createTable.ExecuteReader();
+            }
+        }
+    }
+}
